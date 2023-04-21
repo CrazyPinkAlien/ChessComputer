3c37e529af99a98db510e299982a2c5dad819a3d
Emily Tyler <emily.et@live.com>
2023-04-21 23:42:44 +0100

    Removed pixel perfectness

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use bevy::app::App;
-use bevy::prelude::{ImagePlugin, PluginGroup};
 use bevy::winit::WinitSettings;
 use bevy::DefaultPlugins;
 
@@ -11,7 +10,7 @@ mod ui;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(DefaultPlugins)
         .add_plugin(UIPlugin)
         .add_plugin(CorePlugin)
         .insert_resource(WinitSettings::desktop_app())
